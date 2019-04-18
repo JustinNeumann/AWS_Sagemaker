6e92cc8901fafdbf7da8cd0f9a3e0723156875a3
JustinNeumann <justin.neumann@live.de>
2019-04-18 17:35:29 +0200

    update

--- script.py
+++ script.py
@@ -6,16 +6,16 @@ import argparse
 
 def get_train_data(train_dir):
 
-    X_train = pd.read_csv(os.path.join(train_dir, 'X_train.csv'))
-    y_train = pd.read_csv(os.path.join(train_dir, 'y_train.csv'))
+    X_train = pd.read_csv(os.path.join(train_dir, 'X_train.csv'), header=None)
+    y_train = pd.read_csv(os.path.join(train_dir, 'y_train.csv'), header=None)
     print('X train', X_train.shape,'y train', y_train.shape)
 
     return X_train, y_train
 
 def get_test_data(test_dir):
 
-    X_test = pd.read_csv(os.path.join(test_dir, 'X_test.csv'))
-    y_test = pd.read_csv(os.path.join(test_dir, 'y_test.csv'))
+    X_test = pd.read_csv(os.path.join(test_dir, 'X_test.csv'), header=None)
+    y_test = pd.read_csv(os.path.join(test_dir, 'y_test.csv'), header=None)
     print('X test', X_test.shape,'y test', y_test.shape)
 
     return X_test, y_test
@@ -43,6 +43,9 @@ if __name__ == "__main__":
 	X_train, y_train = get_train_data(args.train)
 	X_test, y_test = get_test_data(args.test)
 	
+	#y_train[0] = pd.to_numeric(y_train[0], downcast='float')
+	#y_test[0] = pd.to_numeric(y_test[0], downcast='float')
+	
 	inputs = tf.keras.Input(shape=(X_train.shape[1],)) # Returns a 'placeholder' tensor
 	x = tf.layers.Dense(32, activation='relu', name = "d1")(inputs)
 	predictions = tf.keras.layers.Dense(1, activation="linear", name = "d2")(x)
@@ -52,7 +55,7 @@ if __name__ == "__main__":
 
 	optimiser = tf.train.GradientDescentOptimizer(learning_rate=0.1)
 
-	model.compile (optimizer= optimiser, loss='mean_squared_error', metrics = ['accuracy'])
+	model.compile (optimizer= optimiser, loss='mean_absolute_error', metrics = ['accuracy'])
 
 	# Store training stats
 	model.fit(X_train, y_train, batch_size=args.batch_size, epochs=args.epochs, validation_data=(X_test, y_test))
